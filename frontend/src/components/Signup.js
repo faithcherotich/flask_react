073da766cb8bbddc75dd050f91c294d7346048ed
faithcherotich <faithcherotich910@gmail.com>
2024-10-24 14:23:55 +0300

    commit

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom'; // Removed Link import
 import './Signup.css';
 
 function Signup() {
